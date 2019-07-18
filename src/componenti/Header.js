ebdd101247b0a070c13939bcafbbe6ffa1b4785a
Ciocci97 <53055284+Ciocci97@users.noreply.github.com>
2019-07-18 19:14:08 +0200

    Update Header.js

--- src/componenti/Header.js
+++ src/componenti/Header.js
@@ -9,16 +9,29 @@ class HeaderComponent extends React.Component {
 		super(props)
 
 		this.handleLogoutClick = this.handleLogoutClick.bind(this)
+        this.handleBibliotecaClick = this.handleBibliotecaClick.bind(this)
+        this.handleSegreteriaClick = this.handleSegreteriaClick.bind(this)
 	}
 
 	handleLogoutClick() {
 		if (this.props.utente) this.props.utente.nome = ''
 		this.props.onHomeClick()
 	}
+    
+    
+    	handleSegreteriaClick() {
+		if (this.props.utente) this.props.utente.nome = ''
+		this.props.onSegreteriaClick()
+	}
+    
+    	handleBibliotecaClick() {
+		if (this.props.utente) this.props.utente.nome = ''
+		this.props.onBibliotecaClick()
+	}
 
 	render() {
 		const title = this.props.utente
-			? 'Benvenuto ' + this.props.utente.nome.charAt(0).toUpperCase() + this.props.utente.nome.slice(1)
+			? 'Benvenuto: ' + this.props.utente.nome.charAt(0).toUpperCase() + this.props.utente.nome.slice(1)
 			: 'Benvenuto'
 
 		return (
@@ -40,8 +53,16 @@ class HeaderComponent extends React.Component {
 					<div className="title">{title}</div>
 					<Menu theme="dark" mode="horizontal" style={{ lineHeight: '64px' }}>
 						<Menu.Item key="1" style={{ float: 'right' }} onClick={this.handleLogoutClick}>
-							Logout
+							Home
 						</Menu.Item>
+                        <Menu.Item key="2" style={{ float: 'right' }} onClick={this.handleSegreteriaClick}>
+							Segreteria
+						</Menu.Item>
+                        
+                        <Menu.Item key="3" style={{ float: 'right' }} onClick={this.handleBibliotecaClick}>
+							Biblioteca
+						</Menu.Item>
+                        
 					</Menu>
 				</Header>
 			</Layout>
